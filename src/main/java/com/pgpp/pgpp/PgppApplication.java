188767d8c47503596bf70960e9cefeb01f01c3ec
Irene De Torres Cifuentes <irenecifuentes@uma.es>
2025-12-16 10:45:42 +0100

    Proyecto Spring Boot inicial

---
+++ src/main/java/com/pgpp/pgpp/PgppApplication.java
@@ -0,0 +1,13 @@
+package com.pgpp.pgpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pgpp/pgpp/PgppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgpp.pgpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
