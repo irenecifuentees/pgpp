f4ba9409dc8f39826f3b7ae4339c5e8f606b6726
Juan Ignacio Quintana Martín <ji.quintanamartin04@gmail.com>
2025-12-16 11:19:08 +0100

    Medicamento añadido

---
+++ src/main/java/com/pgpp/pgpp/model/Medicamento.java
@@ -0,0 +1,45 @@
+package com.pgpp.pgpp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+
+@Entity
+public class Medicamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @ManyToOne
+    //private PosibleTratamiento posibleTratamiento;
+
+    private Long idMedicamento;   
+    private String nombre;
+    private String prActivo;
+
+    public Long getId() {
+        return idMedicamento;
+    }
+
+    public void setId(Long id) {
+        this.idMedicamento = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrActivo() {
+        return prActivo;
+    }
+
+    public void setPrActivo(String prActivo) {
+        this.prActivo = prActivo;
+    }
+}
